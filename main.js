169faaeda6181fc116424befde055c56dd9ddced
hadicodess <85850834+hadicodess@users.noreply.github.com>
2021-10-03 12:40:31 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX = 0; 
+noseY = 0;  
+difference = 0; 
+rightWristX = 0; 
+leftWristX = 0;  
+function setup() 
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500); 
+
+    canvas = createCanvas(550, 500); 
+    canvas.position(560, 150); 
+    poseNet = ml5.poseNet(video, modelLoaded); 
+    poseNet.on('pose', gotPoses); 
+} 
+function draw()
+{
+    background('#2CE863'); 
+    document.getElementById("length"). innerHTML = "width and height of the square is:" + difference +"px"; 
+    fill('#07F3D1'); 
+    stroke('#07F3D1'); 
+    square(noseX, noseY, difference);  
+} 
+function modelLoaded()
+{
+    console.log("PoseNet is initialized!");
+} 
+function gotPoses(results)
+{
+if(results.length > 0)
+{
+    console.log(results);
+    noseX = results[0].pose.nose.x; 
+    noseY = results[0].pose.nose.y;  
+    console.log("noseX = " + noseX +"noseY = " + noseY ); 
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x; 
+difference = floor(leftWristX - rightWristX);  
+console.log("leftWristX + " + leftWristX + "rightWristX = "+ rightWristX + "difference = "+ difference); 
+
+}
+}
